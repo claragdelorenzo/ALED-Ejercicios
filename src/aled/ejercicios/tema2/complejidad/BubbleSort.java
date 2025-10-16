0a184aac19ab2e2d6809975f55f6fe9b4ecea64b
Clara García de Lorenzo <claragdelorenzo@gmail.com>
2025-10-16 11:37:14 +0200

    Correción BubbleSort clase

--- src/aled/ejercicios/tema2/complejidad/BubbleSort.java
+++ src/aled/ejercicios/tema2/complejidad/BubbleSort.java
@@ -2,7 +2,8 @@ package aled.ejercicios.tema2.complejidad;
 
 public class BubbleSort {
 	
-	public static void ordenacionBubble(int[] lista) {
+	// ORDENACION BUBBLESORT COMO ME SALIÓ DEL PAPO (con do-while & CON FLAG)
+	private static void ordenacionBubbleDoWhile(int[] lista) {
 		boolean flag;
 		do {
 			flag = true;
@@ -15,9 +16,27 @@ public class BubbleSort {
 				}
 			}
 			
-			}while(!flag);
+			} while(!flag);
 	}
 	
+	
+	// ORDENACIÓN BUBBLESORT DE CLASE (con x2 for & SIN FLAG)
+	private static void ordenacionBubble(int[] lista) {
+		for (int i = 0; i < lista.length; i++) {
+			for (int j = 0; j < lista.length -1 -i; j++) {
+				// Si es mayor que el segundo, los cambiamos
+				if (lista[j] > lista [j+1]) {
+					// Permutación de elementos
+					int aux = lista[i];
+					lista[i] = lista[i+1];
+					lista[i+1] = aux;
+				}
+			}
+		}
+	}
+	
+	
+	// TOSTRING: para meter en un String los elementos del Array
 	public static String toString (int[] lista) {
 		String text = "";
 		for(int i = 0; i < lista.length; i++)
@@ -25,12 +44,26 @@ public class BubbleSort {
 		return text;
 	}
 
+	
 	public static void main(String[] args) {
-	// Array ORDENADO
-			int[] list = { 5,3,73,92,11,23,1,32,8,66,23,45};		
-			System.out.println("Lista de numeros SIN ORDENAR: " + toString(list) );
-			ordenacionBubble(list);
-			System.out.println("Lista de numeros ORDENADOS: " + toString(list) );
+		
+		// COMPROBACIÓN BUBBLESORT(DOWHILE)
+		// Array 
+		int[] list = { 5,3,73,92,11,23,1,32,8,66,23,45};		
+		// Imprimir array SIN ORDENAR
+		System.out.println("Lista de numeros SIN ORDENAR: " + toString(list) );
+		// Imprimir array ORDENADO
+		ordenacionBubbleDoWhile(list);
+		System.out.println("Lista de numeros ORDENADOS: " + toString(list) );
+		
+		// COMPROBACIÓN BUBBLESORT(DOWHILE)
+		// Array 
+		int[] list2 = { 5,3,73,92,11,23,1,32,8,66,23,45};		
+		// Imprimir array SIN ORDENAR
+		System.out.println("Lista de numeros SIN ORDENAR: " + toString(list) );
+		// Imprimir array ORDENADO
+		ordenacionBubble(list2);
+		System.out.println("Lista de numeros ORDENADOS: " + toString(list) );
 	}
 
 }
